2c016ece14dd3378fb34b275148c4a1e3d3a4838
Jin Xu <s1673820@sms.ed.ac.uk>
2017-06-13 16:10:25 +0100

    move train dn

--- contrib.py
+++ contrib.py
@@ -1,32 +1,9 @@
 import numpy as np
 import tensorflow as tf
-from dependency_network import DependencyNetwork
 import time
 import itertools
 
-def train_dependency_networks(config, num_classes, train_inputs, train_targets, valid_inputs, valid_targets, inputs_block, attr_types, max_num_epoch=500, early_stopping_lookahead=5, quiet=False):
 
-    config['mask_as_inputs'] = config['weights_sharing']
-    graph = tf.Graph()
-    dns = []
-    for i in range(num_classes):
-        dn = DependencyNetwork(inputs_block, attr_types, graph=graph, graph_config=config, name="DN{0}-{1}".format(hash(time.time()), i+1))
-        dns.append(dn)
-    sess = tf.Session(graph=graph)
-    with graph.as_default():
-        init = tf.global_variables_initializer()
-    sess.run(init)
-
-    for dn in dns:
-        dn.set_session(sess)
-
-    valid_errors = []
-    for i, dn in enumerate(dns):
-        e = dn.train(train_inputs[train_targets==i, :], valid_inputs[valid_targets==i, :],
-                 num_epoch=max_num_epoch, batch_size=100, early_stopping_lookahead=early_stopping_lookahead, quiet=quiet)
-        valid_errors.append(e)
-
-    return dns, sess, valid_errors
 
 def compare_datasets(inputs, targets, n_inputs, n_targets):
     data = np.concatenate([inputs, targets[:,None], np.zeros((inputs.shape[0],1), dtype=np.int64)], axis=1)
