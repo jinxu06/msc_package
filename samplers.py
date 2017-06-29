fac59eaac84ff1fb0e7bc279b834272df2f7642c
Jin Xu <s1673820@sms.ed.ac.uk>
2017-06-29 12:03:07 +0100

    sampler for nb

--- samplers.py
+++ samplers.py
@@ -126,6 +126,13 @@ class BlockGibbsSampler(Sampler):
                 s = np.random.poisson(lam=lam[idx])
                 arr.append(s)
             samples[:, block[0]:block[1]] = np.array(arr)[:, None]
+        elif attr_type == 'ib':
+            arr = []
+            for count, prob, alpha in zip(list(cond_prob[0]), list(cond_prob[1]), list(cond_prob[2])):
+                idx = np.argmax(np.random.multinomial(1, pvals=alpha))
+                s = np.random.negative_binomial(n=count[idx], p=prob[idx])
+                arr.append(s)
+            samples[:, block[0]:block[1]] = np.array(arr)[:, None]
         else:
             raise Exception("attr_type not found")
 
